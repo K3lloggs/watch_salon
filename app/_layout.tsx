a4f61ccdd9db562ae9896581794df17796a07e5e
K3lloggs <connorclosesln@gmail.com>
2025-01-28 00:33:18 -0500

    status Bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,13 @@ import { Stack } from 'expo-router';
 import { SortProvider } from './context/SortContext';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { Ionicons } from '@expo/vector-icons';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
   return (
     <FavoritesProvider>
       <SortProvider>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
         <Stack screenOptions={{
           headerLeft: () => <Ionicons name="chevron-back" size={24} color="#000000" />,
           headerBackVisible: false,
